3e95cc7135d51e68b1e4d9dd742bd3c1ad3276b0
ReiJr <reinaldo.isj@hotmail.com>
2018-09-18 14:28:02 -0300

    Update calander.py

--- calander.py
+++ calander.py
@@ -1,41 +1,30 @@
 from __future__ import print_function
-import datetime
-from googleapiclient.discovery import build
+from apiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+GCAL = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
 
+GMT_OFF = '-03:00'      # PDT/MST/GMT-3
+EVENT = {
+    'summary': 'Dinner with friends',
+    'start':  {'dateTime': '2018-09-19T19:00:00%s' % GMT_OFF},
+    'end':    {'dateTime': '2018-09-20T22:00:00%s' % GMT_OFF},
+    'attendees': [
+        {'email': 'reinaldo.sed@gmail.com'},
+    ],
+}
 
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('calendar', 'v3', http=creds.authorize(Http()))
+e = GCAL.events().insert(calendarId='primary',
+        sendNotifications=True, body=EVENT).execute()
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(
-        calendarId='primary',
-        timeMin=now,
-        maxResults=10,
-        singleEvents=True,
-        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-
-
-if __name__ == '__main__':
-    main()
+print('''*** %r event added:
+    Start: %s
+    End:   %s''' % (e['summary'].encode('utf-8'),
+        e['start']['dateTime'], e['end']['dateTime']))
